9d24680a5b8af229d0e70a978c3781a30ff54726
Nathan Hornby <nathan@3degrees>
2014-07-09 18:38:37 +0100

    Removed optional flagging. Shockingly simple. Fixes https://github.com/nathanhornby/hashid_field/issues/8

--- fields/field.hashid_field.php
+++ fields/field.hashid_field.php
@@ -1,5 +1,7 @@
 <?php
 
+	if (!defined('__IN_SYMPHONY__')) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
+
 	require_once EXTENSIONS . '/hashid_field/lib/Hashids.php';
 	require_once FACE . '/interface.exportablefield.php';
 	require_once FACE . '/interface.importablefield.php';
@@ -11,7 +13,7 @@
 		public function __construct(){
 			parent::__construct();
 			$this->_name = __('Hashid');
-			$this->_required = true;
+			$this->_required = false;
 
 			$default_settings = Symphony::Configuration()->get('hashid_field');
 
