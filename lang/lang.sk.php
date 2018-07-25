456b47dbb080c73e232649fc2d45e6ad23195d15
Juraj Kapsz <dev@jurajkapsz.sk>
2018-07-25 15:44:23 +0200

    Added Slovak translation

---
+++ lang/lang.sk.php
@@ -0,0 +1,53 @@
+<?php
+
+	$about = array(
+		'name' => 'Slovensky',
+		'author' => array(
+			'name' => 'Juraj Kapsz',
+			'email' => 'hi@jurajkapsz.sk',
+			'website' => 'http://jurajkapsz.sk'
+		),
+		'release-date' => '2018-07-25'
+	);
+
+	/**
+	 * Hashid Field
+	 */
+	$dictionary = array(
+
+		// Missing
+
+		'Hashid Field' =>
+		'Jedinečný kód (Hashid)',
+
+		'Default salt' =>
+		'Predvolený šifrovací kľúč (salt)',
+
+		'Set to your sitename by default.' =>
+		'Predvolene nastavené podľa názvu webového sídla.',
+
+		'Default hash length' =>
+		'Predvolená dĺžka výsledného jedinečného kódu (hash)',
+
+		'A maximum of 32 characters are saved for each hash.' =>
+		'Každý kód sa uloží v dĺžke najviac 32 znakov.',
+
+		'Hashid' =>
+		'Jedinečný kód (Hashid)',
+
+		'Hash salt' =>
+		'Šifrovací kľúč (salt)',
+
+		'Hash length' =>
+		'Dĺžka jedinečného kódu (hash)',
+
+		'The hash will be generated when the entry is saved.' =>
+		'Kód sa vygeneruje po vytvorení záznamu.',
+
+		'The hash will be replaced when the entry is saved.' =>
+		'Kód sa zamení po uložení záznamu.',
+
+		'Regenerate hash' =>
+		'Obnoviť kód',
+
+	);
